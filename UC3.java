201c57998842a036de084910e619841587bfdf76
sp1371-SRM <sp1371@srmist.edu.in>
2026-02-19 01:21:00 +0530

    UC3 DONE

--- UC3.java
+++ UC3.java
@@ -3,28 +3,20 @@
  * Uses String.join() for memory efficiency.
  */
 public class UC3 {
-
     public static void main(String[] args) {
-        
         // Row 1
-        System.out.println(String.join(" ", "   *** ", "   *** ", " ****** ", "  ***** "));
-        
+        System.out.println(String.join("   ", " *** ", " *** ", "******", " *****"));
         // Row 2
-        System.out.println(String.join(" ", " ** ** ", " ** ** ", " ** ** ", " ** ** "));
-        
+        System.out.println(String.join("   ", "** **", "** **", "** **", "** "));
         // Row 3
-        System.out.println(String.join(" ", " ** ** ", " ** ** ", " ** ** ", " ** "));
-        
+        System.out.println(String.join("   ", "** **", "** **", "** **", "** "));
         // Row 4
-        System.out.println(String.join(" ", " ** ** ", " ** ** ", " ******* ", "  ***** "));
-        
+        System.out.println(String.join("   ", "** **", "** **", "******", " *****"));
         // Row 5
-        System.out.println(String.join(" ", " ** ** ", " ** ** ", " ** ", "      ** "));
-        
+        System.out.println(String.join("   ", "** **", "** **", "**", "     **"));
         // Row 6
-        System.out.println(String.join(" ", " ** ** ", " ** ** ", " ** ", " ** ** "));
-        
+        System.out.println(String.join("   ", "** **", "** **", "**", "** **"));
         // Row 7
-        System.out.println(String.join(" ", "   *** ", "   *** ", " ** ", "  ***** "));
+        System.out.println(String.join("   ", " *** ", " *** ", "**", " *****"));
     }
 }
